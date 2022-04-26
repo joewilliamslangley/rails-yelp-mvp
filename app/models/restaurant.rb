433a6f0be60ced02c6222d1ba1eb37ac32fd14b9
Joe Williams-Langley <joewilliamslangley@gmail.com>
2022-04-26 20:22:53 +0100

    added models

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :category, inclusion: {
+    in: %w[chinese italian japanese french belgian],
+    message: "%{value} is not a valid category."
+  }
+  validates :name, :address, :category, presence: true
+end
